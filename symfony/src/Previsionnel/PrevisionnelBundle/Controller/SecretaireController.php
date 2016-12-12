a98532eeedcc97df1fa76175a2c441434ea46746
Maxime Braud <mbraud@etudiant.univ-lr.fr>
2016-12-12 12:48:25 +0100

    Update module secretaire

--- symfony/src/Previsionnel/PrevisionnelBundle/Controller/SecretaireController.php
+++ symfony/src/Previsionnel/PrevisionnelBundle/Controller/SecretaireController.php
@@ -17,20 +17,23 @@ class SecretaireController extends Controller
     public function indexAction(Request $request)
     {
 
+        $now = new \DateTime("now");
+		$now = $now->format("Y");
+		$anneeScolaire = $now."-".($now + 1);
         /*Récupération de l'entity manager*/
         $em = $this->getDoctrine()->getManager();
         $conn = $em->getConnection();
         $Utilisateurs = new Utilisateurs();
         $form = $this->createForm(Recherche::class, $Utilisateurs);
 
-        /*FAIRE LA RECHERCHE PAR DEPARTEMENTS*/
+        /*Affichage du professeurs recherché*/
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             if($form->get("Rechercher")->isClicked()){
             $Utilisateurs = $form->getData();
-            $sql = "SELECT utilisateurs.id, utilisateurs.login, utilisateurs.nom, utilisateurs.prenom,departements.nom as nomDep FROM utilisateurs
+            $sql = "SELECT utilisateurs.id, utilisateurs.login, utilisateurs.nom, utilisateurs.prenom,departements.nom as nomDep  FROM utilisateurs
         LEFT JOIN archiveutilisateurs ON utilisateurs.id = archiveutilisateurs.idUtilisateur
-        LEFT JOIN departements ON archiveutilisateurs.idDepartement = departements.id WHERE LOWER(utilisateurs.nom) = '".strtolower($Utilisateurs->getNom())."'  ORDER BY nomDep";
+        LEFT JOIN departements ON archiveutilisateurs.idDepartement = departements.id WHERE LOWER(utilisateurs.nom) = '".strtolower($Utilisateurs->getNom())."' AND archiveutilisateurs.annee ='".$anneeScolaire."'  ORDER BY nomDep";
             $res = $conn->query($sql);
             $tab =  array();
             $tab  = $res->fetchAll();
@@ -38,21 +41,19 @@ class SecretaireController extends Controller
         }
         else{
 
-            /*Récupération nom+prenom utlisateur*/
+            /*Affichage des professeurs présent dans la BDD*/
             $sql = "SELECT utilisateurs.id, utilisateurs.login, utilisateurs.nom, utilisateurs.prenom,departements.nom as nomDep FROM utilisateurs
         LEFT JOIN archiveutilisateurs ON utilisateurs.id = archiveutilisateurs.idUtilisateur
-        LEFT JOIN departements ON archiveutilisateurs.idDepartement = departements.id ORDER BY nomDep";
+        LEFT JOIN departements ON archiveutilisateurs.idDepartement = departements.id AND archiveutilisateurs.annee ='".$anneeScolaire."' ORDER BY nomDep";
             $res = $conn->query($sql);
             $tab =  array();
             $tab  = $res->fetchAll();
-
         }
 
         $content = $this
             ->get('templating')
-            ->render('PrevisionnelBundle:Secretaire:index.html.twig' , array('tableauUtilisateur' => $tab , "form" =>$form->createView()));
+            ->render('PrevisionnelBundle:Secretaire:index.html.twig' , array('tableauUtilisateur' => $tab , "form" =>$form->createView() , "annee" => $anneeScolaire ));
 
         return new Response($content);
-    }/*Chercher les formulaire de symfony
-    Essayer requete imbriqué pour les utilisateurs*/
+    }
 }
