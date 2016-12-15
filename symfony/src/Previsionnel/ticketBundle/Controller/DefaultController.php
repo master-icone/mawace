eb255b0449a0ce4bf825a36189ca4b6f5ede5d92
Tsyrosk <jake.map.ff@gmail.com>
2016-12-15 11:27:06 +0100

    Modifications menu et roles Tickets

--- symfony/src/Previsionnel/ticketBundle/Controller/DefaultController.php
+++ symfony/src/Previsionnel/ticketBundle/Controller/DefaultController.php
@@ -18,41 +18,72 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-
-
         $user = $this->getUser();
 
+        $now = new \DateTime("now");
+        $now = $now->format("Y");
+        $anneeScolaire = $now."-".($now + 1);
+
         $em = $this->getDoctrine()->getManager();
-        $idUser = $em->getRepository('Previsionnel\ticketBundle\Entity\Utilisateurs')->findOneBy([
-            "login" => $user]);
+        $utilisateur = $em->getRepository('Previsionnel\PrevisionnelBundle\Entity\Utilisateurs')->findOneBy([
+            "login" => $user
+        ]);
 
+        $listeRoles = array();
+        if (!empty($utilisateur)) {
+            $archive = $em->getRepository('Previsionnel\PrevisionnelBundle\Entity\Archiveutilisateurs')->findOneBy([
+                "idutilisateur" => $utilisateur->getId(),
+                "annee" => $anneeScolaire
+            ]);
 
+            if (!empty($archive)) {
+                $rolesExploded = explode("/", $archive->getIdrole());
 
-            //cree un ticket
-		$ticket = new ticket();
-		
-		//recuperer le formulaire
-		$form = $this->createForm(ticketType::class,$ticket);
+                foreach($rolesExploded as $idrole) {
+                    $role = $em->getRepository('Previsionnel\PrevisionnelBundle\Entity\Roles')->findOneBy([
+                        "id" => $idrole
+                    ]);
 
-        $form->handleRequest($request);
-
-        //si le formulaire a ete soumis
-        if($form->isSubmitted() && $form->isValid()){
-            //enregistrer en BDD
-            $em = $this->getDoctrine()->getManager();
-            $ticket->setIdUE($form->get('idUE')->getData()->getId());
-            $em->persist($ticket);
-
-            $em->flush();
-
-            return new Response('ticket ajoute !');
+                    array_push($listeRoles, $role->getNom());
+                }
+            }
         }
-		
-		//generer le HTML du formulaire
-		$formView = $form->createView();
-		
-		//on rend la vue
-        return $this->render('mawaceticketBundle:Default:index.html.twig',array('form'=>$formView,'idUser'=>$idUser->getId()));
+
+        if (in_array("Envoyer un ticket", $listeRoles)) {
+            //cree un ticket
+            $ticket = new ticket();
+
+            //recuperer le formulaire
+            $form = $this->createForm(ticketType::class,$ticket);
+
+            $form->handleRequest($request);
+
+            //si le formulaire a ete soumis
+            if($form->isSubmitted() && $form->isValid()){
+                //enregistrer en BDD
+                $em = $this->getDoctrine()->getManager();
+                $ticket->setIdUE($form->get('idUE')->getData()->getId());
+                $em->persist($ticket);
+
+                $em->flush();
+
+                return new Response('ticket ajoute !');
+            }
+
+            //generer le HTML du formulaire
+            $formView = $form->createView();
+
+            //on rend la vue
+            return $this->render('mawaceticketBundle:Default:index.html.twig', [
+                'form'=>$formView,
+                'utilisateur'=>$utilisateur,
+                "roles" => $listeRoles,
+                "annee" => $anneeScolaire
+            ]);
+        }
+        else {
+            return $this->redirectToRoute("previsionneluser_accueil");
+        }
     }
     // Autocomplete : Nom de l'UE
 
@@ -79,11 +110,51 @@ class DefaultController extends Controller
 
     public function listeTicketsAction()
     {
-        $ticket = $this->getDoctrine()
-            ->getRepository('Previsionnel\ticketBundle\Entity\ticket')
-            ->findAll();
-        return $this->render('mawaceticketBundle:Default:listeTickets.html.twig',array('listeTickets'=>$ticket));
+        $user = $this->getUser();
 
+        $now = new \DateTime("now");
+        $now = $now->format("Y");
+        $anneeScolaire = $now."-".($now + 1);
+
+        $em = $this->getDoctrine()->getManager();
+        $utilisateur = $em->getRepository('Previsionnel\PrevisionnelBundle\Entity\Utilisateurs')->findOneBy([
+            "login" => $user
+        ]);
+
+        $listeRoles = array();
+        if (!empty($utilisateur)) {
+            $archive = $em->getRepository('Previsionnel\PrevisionnelBundle\Entity\Archiveutilisateurs')->findOneBy([
+                "idutilisateur" => $utilisateur->getId(),
+                "annee" => $anneeScolaire
+            ]);
+
+            if (!empty($archive)) {
+                $rolesExploded = explode("/", $archive->getIdrole());
+
+                foreach($rolesExploded as $idrole) {
+                    $role = $em->getRepository('Previsionnel\PrevisionnelBundle\Entity\Roles')->findOneBy([
+                        "id" => $idrole
+                    ]);
+
+                    array_push($listeRoles, $role->getNom());
+                }
+            }
+        }
+
+        if (in_array("Voir les tickets", $listeRoles)) {
+            $ticket = $this->getDoctrine()
+                ->getRepository('Previsionnel\ticketBundle\Entity\ticket')
+                ->findAll();
+            return $this->render('mawaceticketBundle:Default:listeTickets.html.twig', [
+                'listeTickets'=>$ticket,
+                'utilisateur'=>$utilisateur,
+                "roles" => $listeRoles,
+                "annee" => $anneeScolaire
+            ]);
+        }
+        else {
+            return $this->redirectToRoute("previsionneluser_accueil");
+        }
     }
-	
+
 }
